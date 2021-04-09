8f9a75248304de8da0b82784c4edd3a4d3e1ee8a
Shunsuke Kimura <kimushun1101@gmail.com>
2021-04-10 08:33:02 +0900

    ADD License

--- Gunpey.java
+++ Gunpey.java
@@ -1,3 +1,27 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2021 ShunsukeKimura.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
@@ -29,6 +53,20 @@ class Gunpey extends JFrame {
 		gMenu.iExit.addActionListener(new ActionListener() {
 			public void actionPerformed(final ActionEvent ae) {
 				System.exit(0);
+			}
+		});
+
+		// Action listener of ending of the click
+		gMenu.iHelp.addActionListener(new ActionListener() {
+			public void actionPerformed(final ActionEvent ae) {
+				JFrame frame = new JFrame("ヘルプ");
+				frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				// URL 対応するのが面倒くさくて暫定的に処理してます。
+				// JLabel text = new JLabel("<html>このソフトについては <br><a href='Github'>https://github.com/kimushun1101/gunpey</a><br> をご参照ください。");
+				JLabel text = new JLabel("<html>このソフトについては <br><a href='https://github.com/kimushun1101/gunpey'>https://github.com/kimushun1101/gunpey</a><br> をご参照ください。");
+				frame.getContentPane().add(text);
+				frame.setSize(300, 150);
+				frame.setVisible(true);
 			}
 		});
 
